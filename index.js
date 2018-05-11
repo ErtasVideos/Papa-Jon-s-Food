72efe0603f510d541fe69440c657ccf4af4cab43
ErtasVideos <36131469+ErtasVideos@users.noreply.github.com>
2018-05-11 10:20:44 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const commando = require('discord.js-commando');
+const bot = new commando.Client();
+//Registers of groups
+bot.registry.registerGroup('food', 'Food');
+bot.registry.registerDefaults();
+bot.registry.registerCommandsIn(__dirname + "/commands");
+//Bot tokens
+bot.login(process.env.BOT_TOKEN);
+console.log("started up!")
+bot.syncGuilds
+
+bot.on("ready", () => {
+    bot.user.setStatus('dnd', 'pizza niggas') 
+    bot.user.setGame("INAZUMA COOKERS");
+});
+
