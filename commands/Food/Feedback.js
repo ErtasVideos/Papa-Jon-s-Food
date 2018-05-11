f910febb692cd71835d6c48bd15e9ab3f9b7045e
ErtasVideos <36131469+ErtasVideos@users.noreply.github.com>
2018-05-11 10:26:47 +0200

    Create Feedback.js

---
+++ commands/Food/Feedback.js
@@ -0,0 +1,21 @@
+const commando = require('discord.js-commando');
+var client = new commando.Client();
+var args = args;
+
+class FeedbackCommand extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'feedback',
+           group: 'food',
+         memberName: 'feedback',
+           description: 'Feedback your order!'
+       });
+    }
+
+    async run(message, args) {
+       message.reply("Thanks for the feedback!");
+       this.client.channels.get('444047121503748096').send('Someones Review: ' + args)
+    }
+}
+
+module.exports = FeedbackCommand;
