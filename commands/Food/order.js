dfc957393dfee6eae4196d923e5347b72af4c6d4
ErtasVideos <36131469+ErtasVideos@users.noreply.github.com>
2018-05-13 09:33:11 +0200

    Update order.js

--- commands/Food/order.js
+++ commands/Food/order.js
@@ -1,23 +1,26 @@
-const commando = require('discord.js');
-var client = new Discord.Client();
-var args = args;
+const { Command } = require('discord.js-commando');
 
-class OrderCommand extends Discord.Command {
-        constructor(client) {
-            super(client, {
-                name: 'order',
-                group: 'food',
-                memberName: 'order',
-                description: 'Order Something'
-            });
-        }
+class OrderCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: "order",
+      group: "food",
+      memberName: "order",
+      description: "Order Something",
+    });
+  }
 
-        async run(message, args) {
-            message.reply("Coming right up!");
-                    client.channels.get('432444489773219873').send('Someone Ordered: ' + args)
-                }
-            }
-        
+  async run(message, args) {
+    message.reply("Coming right up!");
+    this.client.channels.get("432444489773219873").send(`Someone Ordered: ${args}`);
+    try {
+      let invite = await message.channel.createInvite();
+      this.client.channels.get("432444489773219873").send(`http://discord.gg/${invite.code}\n**NOTE**: Is not a permanent link`);
+      if(!message.guildmember(client.user).haspermission("CREATE_INVITE")) return message.reply("i dont have perms to create invite so your food cant be delivered!");
+    } catch (err) {
+      console.error(err)
+    }
+  }
+}
 
-
-        module.exports = OrderCommand;
+module.exports = OrderCommand;
