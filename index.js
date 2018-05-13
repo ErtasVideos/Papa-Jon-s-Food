263369f17fcc7abbb9f98c49d9a481626727e79a
ErtasVideos <36131469+ErtasVideos@users.noreply.github.com>
2018-05-13 12:52:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-const commando = require('discord.js');
-const bot = new Discord.Client();
+//Clients for the bot
+const commando = require('discord.js-commando');
+const bot = new commando.Client();
+
 //Registers of groups
 bot.registry.registerGroup('food', 'Food');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands");
 //Bot tokens
-bot.login(process.env.BOT_TOKEN);
+bot.login('');
 console.log("started up!")
 bot.syncGuilds
-
+//Bot Status
 bot.on("ready", () => {
-    bot.user.setStatus('dnd', 'pizza niggas') 
-    bot.user.setGame("INAZUMA COOKERS");
+    bot.user.setStatus('dnd') 
+    bot.user.setActivity('Cooking Tutorials', { type: 'WATCHING' })
+  .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
+  .catch(console.error);
+    
 });
+
+
 
